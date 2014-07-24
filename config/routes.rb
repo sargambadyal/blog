7766eb85aec800e8ff99be81c16d5c6e2df8deb2
Sargam Badyal <sera.bravo004@gmail.com>
2014-07-24 12:56:37 +0530

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Blog::Application.routes.draw do
 
+
   resources :users
+  root 'users'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
