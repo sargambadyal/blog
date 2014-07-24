3460f513a0f9d3171cc2e030c155741d9b0a4d8e
Sargam Badyal <sera.bravo004@gmail.com>
2014-07-24 12:59:38 +0530

    undo routing changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Blog::Application.routes.draw do
 
 
   resources :users
-  root 'users'
+  #root 'users'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
