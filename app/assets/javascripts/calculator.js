de5d6ec46559747c6f54824a8898e31545017dbc
Sargam Badyal <sera.bravo004@gmail.com>
2014-07-29 11:26:18 +0530

    two calculators observer pattern

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -3,24 +3,27 @@ var Calculator = function (commandHistory, form) {
     this.viewElement = $("#" + form);
     this.button = this.viewElement.find(".myButton");
     this.command = this.viewElement.find(".myInput");
+    this.observers = $({});
     //console.log(this.command);
     this.initialize();
-    this.click();
+
 
 };
 
 Calculator.prototype = {
     initialize: function () {
         //alert("initialize");
+        this.click();
         $.ajax({
             url: "api/calculator_create",
             method: "POST"
         }).done(_.bind(f, this));
     },
-
+    
     click: function () {
         var self = this;
         $(this.button).click(function () {
+
             console.log("clicked button");
             console.log("this");
             console.log(this);
@@ -30,6 +33,7 @@ Calculator.prototype = {
             console.log(self.command);
             console.log(self.command.val());
             self.calculate(self.command.val());
+
 
         });
 
@@ -45,13 +49,32 @@ Calculator.prototype = {
         }).success(_.bind(f2, this))
 
     },
-    append_command_history: function (state) {
+    append_command_history: function (state, command) {
         //alert("appending history..");
-        var displayStatement = "<h4>Command was : " + $(this.command).val() + " Result was : " + (state == null ? "Undefined Operation" : state) + "</h4>";
+        var displayStatement = "<h4>Command was : " + $(command).val() + " Result was : " + (state == null ? "Undefined Operation" : state) + "</h4>";
         console.log("append_command_history");
         console.log(displayStatement);
-        this.commandHistoryId.append(displayStatement);
+        this.extract(displayStatement);
+
+    },
+    extract: function (display) {
+        console.log("this is display block");
+        this.commandHistoryId.append(display);
+//        this.notify(display);
+
+
+    },
+    registerObserver: function (calc) {
+        console.log("this is register observers");
+        this.observers.bind('update:history', function (event, state, command) {
+            calc.append_command_history(state, command);
+        });
+    },
+    notify: function (state, command) {
+        console.log("this is trigger block");
+        this.observers.trigger('update:history', [state, command]);
     }
+
 };
 
 
@@ -65,7 +88,8 @@ var f = function (data, statusText, xhr) {
 var f2 = function (data) {
     console.log("data");
     console.log(data);
-    this.append_command_history(data.state);
+    this.append_command_history(data.state, this.command);
+    this.notify(data.state, this.command);
 };
 
 
@@ -73,6 +97,14 @@ $(document).ready(function () {
     console.log("inside ready")
     var calculator = new Calculator("myHistory", "myForm");
     var calculator1 = new Calculator("myHistory1", "myForm1");
+
+
+    console.log(calculator.observers);
+    console.log(calculator1.observers);
+
+    calculator.registerObserver(calculator1);
+    calculator1.registerObserver(calculator);
+
 
 });
 
